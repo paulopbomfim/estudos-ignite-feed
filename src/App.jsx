ca94f20d2cdb62c7347454ef4fafbdeddcdb351a
Paulo Henrique Bomfim <paulopbomfim@gmail.com>
2022-06-20 14:17:22 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
+import { Post } from "./components/Post"
+
+import styles from './App.module.css'
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={ styles.wrapper }>
+        <Sidebar />
+        <main>
+          <Post />
+        </main>
+      </div>
+    </div>
+  )
+}
